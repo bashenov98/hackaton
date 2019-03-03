bdea475fb184ecc25f08a94954a46a784c200726
bashenov98 <35542809+bashenov98@users.noreply.github.com>
2019-03-03 14:55:31 +0600

    Add files via upload

---
+++ hackathon v2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  hackathon v2
+//
+//  Created by Meiir Nurzhanov on 3/3/19.
+//  Copyright © 2019 Meko_n1. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ hackathon v2/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  hackathon v2
+//
+//  Created by Meiir Nurzhanov on 3/3/19.
+//  Copyright © 2019 Meko_n1. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
